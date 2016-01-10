39fafbc03808e18cff2aff68cbf27b055b2cb8f5
Kazutoshi <tanaka@isi.imi.i.u-tokyo.ac.jp>
2016-01-10 22:04:00 +0900

    wbi for athletebot ver. 1st

---
+++ wbi/build/CMakeFiles/WBI_example.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/k-tanaka/Code/kzts/wbi/Module.cpp" "/home/k-tanaka/Code/kzts/wbi/build/CMakeFiles/WBI_example.dir/Module.cpp.o"
+  "/home/k-tanaka/Code/kzts/wbi/main.cpp" "/home/k-tanaka/Code/kzts/wbi/build/CMakeFiles/WBI_example.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "YARP_NO_DEPRECATED_WARNINGS"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  ".."
+  "/usr/local/include"
+  "/usr/include/eigen3"
+  "/home/k-tanaka/Code/codyco-superbuild/build/install/include"
+  "/home/k-tanaka/Code/codyco-superbuild/build/install"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ wbi/build/CMakeFiles/WBI_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/WBI_example.dir/main.cpp.o"
+  "CMakeFiles/WBI_example.dir/Module.cpp.o"
+  "WBI_example.pdb"
+  "WBI_example"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/WBI_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
